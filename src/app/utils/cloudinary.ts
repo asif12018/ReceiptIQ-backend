28f3de025100cd8fd4449b00dcba7007ac9882b1
asif12018 <asif.request@gmail.com>
2026-05-12 06:51:43 +0600

    minor change

--- src/app/utils/cloudinary.ts
+++ src/app/utils/cloudinary.ts
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from "cloudinary";
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET || process.env.CLOUDINARY_SECRET
 });
 
 export const uploadToCloudinary = async (imageBuffer: Buffer, mimeType: string): Promise<string> => {
